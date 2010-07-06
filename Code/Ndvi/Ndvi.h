b96b9b927736e33f9b32d3bf001124ec5e366327
tclarke <tclarke@2d91f92e-8166-0410-902e-d52292f61e86>
2010-07-06 13:57:29 +0000

    EXTRAS-63:Add an NDVI tool which selects bands based on wavelength information.
    git-svn-id: svn://svn.code.sf.net/p/opticks-extras/code/Spectral/trunk/future@986 2d91f92e-8166-0410-902e-d52292f61e86

---
+++ Code/Ndvi/Ndvi.h
@@ -0,0 +1,26 @@
+/*
+ * The information in this file is
+ * Copyright(c) 2010 Ball Aerospace & Technologies Corporation
+ * and is subject to the terms and conditions of the
+ * GNU Lesser General Public License Version 2.1
+ * The license text is available from   
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+
+#ifndef NDVI_H__
+#define NDVI_H__
+
+#include "AlgorithmShell.h"
+
+class Ndvi : public AlgorithmShell
+{
+public:
+   Ndvi();
+   virtual ~Ndvi();
+
+   virtual bool getInputSpecification(PlugInArgList*& pInArgList);
+   virtual bool getOutputSpecification(PlugInArgList*& pOutArgList);
+   virtual bool execute(PlugInArgList* pInArgList, PlugInArgList* pOutArgList);
+};
+
+#endif
